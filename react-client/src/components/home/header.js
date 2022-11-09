685061026ac6c5afd06ed0c9155df0462964e4fb
khafidf <khfd027@gmail.com>
2022-11-09 19:32:53 +0700

    update home

--- react-client/src/components/home/header.js
+++ react-client/src/components/home/header.js
@@ -6,7 +6,7 @@ const Header = () => {
             <div className="container px-4 px-lg-5 text-center">
                 <h1 className="mb-1"><span className="text-primary">D</span>'Attendance</h1>
                 <h3 className="mb-5"><em>A Website It Easy to Record Your Attendance</em></h3>
-                <a className="btn btn-primary btn-xl" href="/login">Make Your Account {<AiOutlineArrowRight />}</a>
+                <a className="btn btn-primary btn-xl" href="/register">Make Your Account {<AiOutlineArrowRight />}</a>
             </div>
         </div>
     );
