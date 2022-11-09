a46266fdc22694fb52052219ed13a01680d5e770
khafidf <khfd027@gmail.com>
2022-11-09 20:00:10 +0700

    add icons

--- react-client/src/components/home/note.js
+++ react-client/src/components/home/note.js
@@ -1,9 +1,15 @@
+import { GiBurningDot } from "react-icons/gi";
+import { IconContext } from "react-icons";
+
 const Note = () => {
     return (
         <div className="d-flex align-items-center" style={{ height: "80vh" }}>
             <div className="container px-4 px-lg-5 text-center">
-                <h3 className="mb-1">Note :</h3>
-                <h5 className="mb-5"><em>if you register with "admin" name, then you will get different features</em></h5>
+                <h3 className="mb-4">Note :</h3>
+                <IconContext.Provider value={{ style: { verticalAlign: "middle", marginBottom: "0.2em", width: "2em" } }}>
+                    <h5 className="mb-2">{<GiBurningDot />} <em>If you register with "admin" name, then you will get different features</em></h5>
+                    <h5>{<GiBurningDot />} <em>Express-api running on port 3001</em></h5>
+                </IconContext.Provider>
             </div>
         </div>
     );
